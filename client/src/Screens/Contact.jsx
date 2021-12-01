c5c4a5a04770c6b73e75bdc8b55c0a25c5db20af
Joseph McElroy <66266130+JosephMcElroy83@users.noreply.github.com>
2021-11-30 19:46:31 -0500

    update changes in basic css design and contact form reworking in progress

--- client/src/Screens/Contact.jsx
+++ client/src/Screens/Contact.jsx
@@ -1,10 +1,27 @@
 import Layout from "../Layout/Layout";
+import linkedinIcon from "../assets/linkedin.svg"
+import githubIcon from "../assets/github.png"
+import "./Contact.css"
 
 export default function Contact() {
   return (
     <Layout>
-      <div>
-        Contact Page
+      <div className="contact-screen">
+        <div className="contact-info">
+          <h2>Info Info Info</h2>
+        </div>
+        <div className="form-container">
+        </div>
+        <div className="clickable-icons">
+          <a href="https://github.com/JosephMcElroy83" className="github">
+          <img className="contact-imgs" src={githubIcon} alt="github" />
+          </a>
+          <a href="https://www.linkedin.com/in/joseph-mcelroy-ba73161a1/" className="linkedin">
+          <img className="contact-imgs" src={linkedinIcon} alt="linkedin" />
+          </a>
+          
+          <img className="contact-imgs" src="https://img.icons8.com/ios-filled/100/000000/gmail.png" alt="email"/>
+        </div>
       </div>
     </Layout>
   )

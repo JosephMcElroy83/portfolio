8e2414bc95ed0890ef4ee55c95713671c9413cc3
Joseph McElroy <66266130+JosephMcElroy83@users.noreply.github.com>
2021-12-01 21:01:34 -0500

    updated minor css changes, added hover effects to contact page to isolate buttons

--- client/src/Screens/Contact.jsx
+++ client/src/Screens/Contact.jsx
@@ -7,8 +7,12 @@ export default function Contact() {
   return (
     <Layout>
       <div className="contact-screen">
+        <div className="personal-info">
+          <img className="profile-img" src="https://media-exp1.licdn.com/dms/image/C4D03AQGgCZLjTS1Uxw/profile-displayphoto-shrink_400_400/0/1580943885737?e=1643846400&v=beta&t=K2g05pZJaLquq5ZCNhMTvT06ISm7cyDf2HtN9gqE3YY" alt="linkedin-profile-pic" />
+          <h2 className="name">Joseph McElroy</h2>
+        </div>
         <div className="contact-info">
-          <h2>Info Info Info</h2>
+          <h2 className="contact-text">Linkedin is the best way of quick contact</h2>
         </div>
         <div className="form-container">
         </div>
@@ -20,7 +24,7 @@ export default function Contact() {
           <img className="contact-imgs" src={linkedinIcon} alt="linkedin" />
           </a>
           
-          <img className="contact-imgs" src="https://img.icons8.com/ios-filled/100/000000/gmail.png" alt="email"/>
+          {/* <img className="contact-imgs" src="https://img.icons8.com/ios-filled/100/000000/gmail.png" alt="email"/> */}
         </div>
       </div>
     </Layout>
